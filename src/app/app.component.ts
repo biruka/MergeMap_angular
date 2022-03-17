839aa29b1f43d5a2a2463077deeea4b2206d7e35
biruka <biras7070@gmail.com>
2022-03-17 21:00:04 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { of, from, fromEvent, interval, Observable } from 'rxjs';
+import { mergeMap, map, catchError } from 'rxjs/operators';
+ 
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  @ViewChild('button',{static:true}) button;
+  clicks$:Observable<any>;
+  count : number = 0;
+  ngAfterViewInit() {
+    this.clicks$ = fromEvent(this.button.nativeElement, 'click');
+    this.mergeMapExample();
+  }
+ 
+  delayedObs(count:number) {
+    return new Observable((observer) => {
+      setTimeout(() => { observer.next(count+" A") }, 1000);
+      setTimeout(() => { observer.next(count+" B") }, 2000);
+      setTimeout(() => { observer.next(count+" C") }, 3000);
+      setTimeout(() => { observer.next(count+" D") }, 4000);
+      setTimeout(() => { observer.next(count+" E"); observer.complete() }, 5000);
+    })
+  }
+ 
+  mergeMapExample() {
+ 
+    let obs=
+ 
+    this.clicks$
+      .pipe(
+        mergeMap(() => {
+          this.count=this.count+1;
+          return this.delayedObs(this.count)
+        })
+      )
+      .subscribe(val => console.log(val));
+  }
+}
